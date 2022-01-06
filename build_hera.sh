4df818e8a0ddf7ce650383a104c877b0553e7928
Dustin Swales <dustin.swales@noaa.gov>
2022-01-06 21:55:34 +0000

    Cleanup makefile

--- build_hera.sh
+++ build_hera.sh
@@ -6,15 +6,22 @@ ml netcdf/4.7.0
 ml
 
 export FC=ifort
-export FCFLAGS="-m64 -O3 -g -traceback -heap-arrays -assume realloc_lhs -extend-source 132 -shared -fpic"
+export FCFLAGS="-m64 -O3 -g -traceback -heap-arrays -assume realloc_lhs -extend-source 132"
+export FCFLAGSS="-shared -fpic"
 export NFHOME=/apps/netcdf/4.7.0/intel/18.0.5.274
 export NCHOME=${NFHOME}
 export RRTMGP_ROOT=/home/Dustin.Swales/Projects/radiation-nn/rrtmgp-nn-training/rte-rrtmgp
 
-cd rte-rrtmgp
-make clean
-make libs
+# RRTMGP libraries
+#cd rte-rrtmgp
+#make clean
+#make libs
+#cd ../
 
-cd build
-${FC} ${FCFLAGS} mo_rte_kind.o mo_rte_util_array.o  ${RRTMGP_ROOT}/rrtmgp/kernels/mo_gas_optics_kernels.F90 -o mo_gas_optics_kernels.so
-cd ../../
+# gas-optics kernels shared library
+mkdir -p rte-rrtmgp/build/libss
+cd rte-rrtmgp/build/libss
+${FC} ${FCFLAGS} ${FCFLAGSS} -c ${RRTMGP_ROOT}/rte/mo_rte_kind.F90
+${FC} ${FCFLAGS} ${FCFLAGSS} -c ${RRTMGP_ROOT}/rte/mo_rte_util_array.F90
+${FC} ${FCFLAGS} ${FCFLAGSS} mo_rte_kind.o mo_rte_util_array.o  ${RRTMGP_ROOT}/rrtmgp/kernels/mo_gas_optics_kernels.F90 -o mo_gas_optics_kernels.so
+cd ../../../
